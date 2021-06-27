6e4cd376626b9ea43de65b57c198f26e196d0961
Greysnek <semenovivm@gmail.com>
2021-06-27 19:03:54 +0300

    Audio conductor with testing rhythmic movement

---
+++ Assets/Scripts/AudioTimeConductor.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioTimeConductor : MonoBehaviour
+{
+    [NonSerialized] public static AudioTimeConductor Main;
+    
+    public float beatsPerMinute;
+    public float firstBeatOffset;
+    [SerializeField] private bool playOnAwake;
+    [SerializeField] private UnityEvent beat;
+    
+    public float SongPositionInSeconds
+    {
+        get
+        {
+            if (_isPlaying)
+            {
+                return (float) (AudioSettings.dspTime - _dspSongTime - firstBeatOffset);
+            }
+
+            return 0;
+        }
+    }
+    public float SongPositionInBeats
+    {
+        get
+        {
+            if (SecPerBeat != 0)
+            {
+                return SongPositionInSeconds / SecPerBeat;
+            }
+            
+            Debug.LogError("Your beat too fast :)");
+            
+            return 0;
+        }
+    }
+    public float SongDeltaTime => SongPositionInSeconds - _oldSongPosition;
+    public float SecPerBeat { get; private set; }
+
+    
+    private AudioSource _musicSource;
+    private bool _isPlaying;
+    private IEnumerator _playing;
+    private float _dspSongTime;
+    private float _oldSongPosition;
+    private float _currentAfterBeatTime;
+    
+
+    private void Start()
+    {
+        Main = this;
+        
+        _musicSource = GetComponent<AudioSource>();
+        _musicSource.playOnAwake = false;
+
+        if (playOnAwake)
+        {
+            Play();
+        }
+    }
+
+    public void Play()
+    {
+        SecPerBeat = 60f / beatsPerMinute;
+        _dspSongTime = (float) AudioSettings.dspTime;
+        
+        _isPlaying = true;
+        _musicSource.Play(0);
+
+        _currentAfterBeatTime = 0;
+    }
+
+    private void Update()
+    {
+        
+        if (SongPositionInSeconds < _musicSource.clip.length)
+        {
+            _currentAfterBeatTime += Time.deltaTime;
+            if (!(_currentAfterBeatTime > SecPerBeat)) return;
+            _currentAfterBeatTime = 0;
+            
+            beat?.Invoke();
+
+            return;
+        }
+
+        _isPlaying = false;
+    }
+    
+    private void LateUpdate()
+    {
+        _oldSongPosition = SongPositionInSeconds;
+    }
+
+}
---
+++ Assets/Scripts/TestMovement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    private Vector3 Scale => _isOddBeat ? Vector3.one : Vector3.one * -1;
+
+    private bool _isOddBeat;
+
+    [SerializeField] private float speed = 2;
+
+    private float _oldBeatTime;
+
+    public void SwitchScale()
+    {
+        _isOddBeat = !_isOddBeat;
+
+        var error = Mathf.Abs(Time.time - _oldBeatTime - AudioTimeConductor.Main.SecPerBeat);
+        _oldBeatTime = Time.time;
+        
+        if (error > 0.0085)
+        {
+            Debug.LogWarning("accuracy: +-" + error);
+        }
+        else
+        {
+            Debug.Log("accuracy: +-" + error);
+        }   
+    }
+    public void Update()
+    {
+        transform.localScale += Scale * speed * AudioTimeConductor.Main.SongDeltaTime / AudioTimeConductor.Main.SecPerBeat;
+    }
+}
